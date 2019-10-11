9ac70a3434279813f5bebfca54e39a7077b21138
Theo Chatzimichos <tampakrap@gmail.com>
2019-10-11 22:16:57 +0200

    add the exact time on the countdown

--- js/script.js
+++ js/script.js
@@ -470,7 +470,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/07/11', function(event) {
+        $('#clock').countdown('2020/07/11 19:00:00', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
