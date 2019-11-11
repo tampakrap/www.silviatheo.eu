8869147d0a58635a2af7f858a5bb7f821f5bc9f5
Theo Chatzimichos <tampakrap@gmail.com>
2019-11-11 19:42:14 +0100

    bin/install.sh: do not install files/dirs that are already installed

--- bin/install.sh
+++ bin/install.sh
@@ -9,9 +9,14 @@ FILES=(
     index.html
 )
 
+case $OSTYPE in
+    darwin*) CP=/usr/local/bin/gcp ;;
+    *) CP=/usr/bin/cp ;;
+esac
+
 mkdir -p dist
 
 for f in ${FILES[@]}; do
     [[ $f =~ "/" ]] && mkdir -p dist/${f%%/*}
-    gcp -rp --preserve=links src/$f dist/$f
+    test -e dist/$f || $CP -rp --preserve=links src/$f dist/$f
 done
