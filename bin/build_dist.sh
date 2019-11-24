f3f498317dbff04da2366b8a8ad79c9181b6c5ea
Theo Chatzimichos <tampakrap@gmail.com>
2019-11-24 23:41:50 +0100

    move the CNAME inside src
    it is not needed to be on the top dir on the master branch, it needs to
    be on the top dir on the gh-pages branch, which is the one getting
    deployed. This also removes one line from the script

--- bin/build_dist.sh
+++ bin/build_dist.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 FILES=(
+    CNAME
     css/style.min.css
     css/style.min.css.map
     el
@@ -29,7 +30,5 @@ for f in ${FILES[@]}; do
     [[ $f =~ "/" ]] && mkdir -p dist/${f%%/*}
     test -e dist/$f || $CP -rp --preserve=links src/$f dist/$f
 done
-
-$CP CNAME dist/
 
 $SED -i -e 's#\.\./\(css\|fonts\|images\|js\)#/\1#g' dist/css/style.min.css dist/js/script.min.js dist/{cz,de,en,es,gr}/index.html
